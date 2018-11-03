d096a71ff49fd29b673ad1ba8056c14d069a73e7
Liam Loxton <liam.loxton@eventbase.com>
2018-11-02 18:40:50 -0700

    Initial Commit

---
+++ BeaconOrgTool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeaconOrgTool
+//
+//  Created by Eventbase on 2018-11-02.
+//  Copyright © 2018 Eventbase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
